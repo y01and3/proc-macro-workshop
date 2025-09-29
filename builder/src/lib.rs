31613f2aa0061444b7440bd8fde2fb71ce4662e7
y01and3 <1791280644@qq.com>
2025-09-29 21:10:52 +0800

    Solve builder 02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,62 @@
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let attrs = input.attrs;
+    let vis = input.vis;
+    let ident = input.ident;
+    let new_ident = Ident::new(&format!("{}Builder", ident), Span::call_site());
+    let generics = input.generics;
+    if let Data::Struct(data) = input.data {
+        let fields = data
+            .fields
+            .iter()
+            .map(|field| {
+                let attrs = &field.attrs;
+                let vis = &field.vis;
+                let ident = &field.ident;
+                let ty = &field.ty;
 
-    TokenStream::new()
+                return quote! {
+                    #(#attrs)*
+                    #vis #ident: Option<#ty>,
+                };
+            })
+            .collect::<Vec<proc_macro2::TokenStream>>();
+
+        let data_fields = data
+            .fields
+            .iter()
+            .map(|field| {
+                let ident = &field.ident;
+
+                return quote! {
+                    #ident: None,
+                };
+            })
+            .collect::<Vec<proc_macro2::TokenStream>>();
+
+        let expanded = quote! {
+                #(#attrs)*
+                #vis struct #new_ident #generics {
+                    #(#fields)*
+                }
+
+                impl #ident {
+                    pub fn builder() -> #new_ident {
+                        #new_ident {
+                            #(#data_fields)*
+                        }
+                    }
+                }
+            };
+
+        TokenStream::from(expanded)
+    } else {
+        panic!()
+    }
 }
